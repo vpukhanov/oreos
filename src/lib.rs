0cc6c5a759af2d8cb23ccd7b68758d73050ae25c
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2020-07-05 11:31:34 +0300

    adds double fault handling

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@
 
 use core::panic::PanicInfo;
 
+pub mod gdt;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
@@ -21,6 +22,7 @@ pub extern "C" fn _start() -> ! {
 }
 
 pub fn init() {
+    gdt::init();
     interrupts::init_idt();
 }
 
--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,12 @@ pub extern "C" fn _start() -> ! {
 
     oreos::init();
 
-    // Invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    fn stack_overflow() {
+        stack_overflow();
+    }
+
+    // trigger a stack overflow
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
